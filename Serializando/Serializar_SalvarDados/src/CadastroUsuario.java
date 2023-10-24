6abd80ab266072f171bf8b4a7e662c1676f1b959
HFVostok <hiago.fernandor7@gmail.com>
2023-10-23 21:52:50 -0300

    23/10/2023.1

---
+++ Serializando/Serializar_SalvarDados/src/CadastroUsuario.java
@@ -0,0 +1,130 @@
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+;
+
+public class CadastroUsuario extends JFrame {
+
+    private JTextField inputNome;
+    private JTextField inputIdade;
+    private DefaultTableModel tableModel;
+    private JTable table;
+
+    private List<User> usuarios = new ArrayList<>();
+    private int linhaSelecionada = -1;
+
+    public CadastroUsuario() {
+
+        setTitle("Cadastro de Usuários");
+        setSize(600, 300);
+        setDefaultCloseOperation(2);
+        setLocationRelativeTo(null);
+
+        // set do Frame
+
+        tableModel = new DefaultTableModel();
+        tableModel.addColumn("Nome");
+        tableModel.addColumn("Idade");
+        table = new JTable(tableModel);
+        JScrollPane scrollPane = new JScrollPane(table); // barra de rolagem
+
+        /// declaração dos Componentes
+
+        inputNome = new JTextField(20);
+        inputIdade = new JTextField(5);
+        JButton cadastrarButton = new JButton("Cadastrar");
+        JButton atualizarButton = new JButton("Atualizar");
+        JButton apagarButton = new JButton("Apagar");
+        JButton apagarTodosButton = new JButton("Apagar Todos");
+        JButton salvarButton = new JButton("Salvar");
+        JPanel inputPanel = new JPanel();
+        inputPanel.add(new JLabel("Nome:"));
+        inputPanel.add(inputNome);
+        inputPanel.add(new JLabel("Idade:"));
+        inputPanel.add(inputIdade);
+        inputPanel.add(cadastrarButton);
+        inputPanel.add(atualizarButton);
+        inputPanel.add(apagarButton);
+        inputPanel.add(apagarTodosButton);
+        inputPanel.add(salvarButton);
+
+        // set do Layout
+
+        setLayout(new BorderLayout());
+        add(inputPanel, BorderLayout.NORTH);
+        add(scrollPane, BorderLayout.CENTER);
+
+        File arquivo = new File("dados.txt");
+        if (arquivo.exists()) {
+            usuarios = Serializacao.deserializar("dados.txt");
+            atualizarTabela();
+        }
+        table.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent evt) {
+                linhaSelecionada = table.rowAtPoint(evt.getPoint());
+                if (linhaSelecionada != -1) {
+                    inputNome.setText((String) table.getValueAt(linhaSelecionada, 0));
+                    inputIdade.setText(table.getValueAt(linhaSelecionada, 1).toString());
+
+                }
+            }
+        });
+        OperacoesUsuario operacoes = new OperacoesUsuario(usuarios, tableModel, table);
+        cadastrarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                operacoes.cadastrarUsuario(inputNome.getText(), inputIdade.getText());
+                inputNome.setText("");
+                inputIdade.setText("");
+            }
+        });
+        atualizarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                operacoes.atualizarUsuario(linhaSelecionada, inputNome.getText(),
+
+                        inputIdade.getText());
+
+            }
+        });
+        apagarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                operacoes.apagarUsuario(linhaSelecionada);
+            }
+        });
+        apagarTodosButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                operacoes.apagarTodosUsuarios();
+            }
+        });
+        salvarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                operacoes.salvarUsuarios();
+            }
+        });
+    }
+
+    private void atualizarTabela() {
+        tableModel.setRowCount(0);
+        for (User usuario : usuarios) {
+            tableModel.addRow(new Object[] { usuario.getNome(), usuario.getIdade() });
+        }
+    }
+
+    public void run (){
+        pack();
+        setVisible(true);
+    }
+}
---
+++ Serializando/Serializar_SalvarDados/src/OperacoesUsuario.java
@@ -0,0 +1,54 @@
+import java.util.List;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+public class OperacoesUsuario {
+    private List<User> usuarios;
+    private DefaultTableModel tableModel;
+    private JTable table;
+
+    public OperacoesUsuario(List<User> usuarios, DefaultTableModel tableModel, JTable table) {
+        this.usuarios = usuarios;
+        this.tableModel = tableModel;
+        this.table = table;
+    }
+
+    public void cadastrarUsuario(String nome, String idade) {
+        int idadeInt = Integer.parseInt(idade);
+        User usuario = new User(nome, idadeInt);
+        usuarios.add(usuario);
+        atualizarTabela();
+    }
+
+    public void atualizarUsuario(int linhaSelecionada, String nome, String idade) {
+        if (linhaSelecionada != -1) {
+            int idadeInt = Integer.parseInt(idade);
+            User usuario = new User(nome, idadeInt);
+            usuarios.set(linhaSelecionada, usuario);
+            atualizarTabela();
+        }
+    }
+
+    public void apagarUsuario(int linhaSelecionada) {
+        if (linhaSelecionada != -1) {
+            usuarios.remove(linhaSelecionada);
+            atualizarTabela();
+        }
+    }
+
+    public void apagarTodosUsuarios() {
+        usuarios.clear();
+        atualizarTabela();
+    }
+
+    public void salvarUsuarios() {
+        Serializacao.serializar("dados.txt", usuarios);
+    }
+
+    private void atualizarTabela() {
+        tableModel.setRowCount(0);
+        for (User usuario : usuarios) {
+            tableModel.addRow(new Object[] { usuario.getNome(), usuario.getIdade() });
+        }
+    }
+}
